85bbee74a933e6b76f6c3be33a6886d6dbbd2756
JadeRabbit <yuheng_wu@sfu.ca>
2020-01-22 23:09:22 -0800

    Coding Standard changes, and dummy game selection

--- tools/GameServer/GameServer.cpp
+++ tools/GameServer/GameServer.cpp
@@ -9,7 +9,6 @@ std::vector<networking::Connection> clients;
 
 void onDisconnect(networking::Connection);
 void onConnect(networking::Connection);
-std::string getHTTPMessage(const char*);
 
 int main(int argc, const char* argv[]){
 	
@@ -22,34 +21,50 @@ int main(int argc, const char* argv[]){
 
 	std::string port = configuration["DefaultPort"];
 	std::string htmlpath = configuration["HTML Location"];
-	
 
 	std::cout << "starting server \nport: " << port << "\nhtml path: " << htmlpath << '\n';
 
 	//Configure Server
 	//string to short conversion check
 	//html path check for valid file
-    networking::Server server{std::stoi(port), htmlpath, onConnect, onDisconnect};
+    networking::Server server{std::stoi(port), htmlpath, OnConnect, OnDisconnect};
 
+	//Select Game
+    std::string games[] {"game1.json", "game2.json", "game3.json"}; //keep this in json as well?
+    std::cout << "\nPlease select a game\n1. Game 1\n2. Game 2\n3. Game 3\n";
+    int gameIndex;
 
-	//Select game and prepare lobby
-	//Until "Start"
+    do{
+    	std::cin >> gameIndex;
+	}while(gameIndex <= 0 || gameIndex > 3);
+
+ 	std::cout << "Selected Game: " << games[gameIndex-1] << "\n";
+
+    //Prepare Lobby in background
+
+    //wait for "start game"?
+
+    //get all users
+
+    //start game with users
+
 	//Wait until End signal
-	//reselect game
+
+	//select another game
 
 	return 0;
 }
 
 //teacher provided functions
 void
-onConnect(networking::Connection c) {
+OnConnect(networking::Connection c) {
   std::cout << "New connection found: " << c.id << "\n";
   clients.push_back(c);
 }
 
 //teacher provided functions
 void
-onDisconnect(networking::Connection c) {
+OnDisconnect(networking::Connection c) {
   std::cout << "Connection lost: " << c.id << "\n";
   auto eraseBegin = std::remove(std::begin(clients), std::end(clients), c);
   clients.erase(eraseBegin, std::end(clients));
