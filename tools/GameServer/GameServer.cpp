ccfbb8dcb3216746d3cf3a72e3846eb7e22343fd
JadeRabbit <yuheng_wu@sfu.ca>
2020-01-23 19:48:50 -0800

    Refactored design based on new information + input sanity checks

--- tools/GameServer/GameServer.cpp
+++ tools/GameServer/GameServer.cpp
@@ -4,68 +4,74 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unistd.h>
 
-std::vector<networking::Connection> clients;
+static const std::string SERVER_CONFIGURATION_FILE_LOCATION = "config/ServerProperties.json";
+static std::vector<networking::Connection> clients;
 
-void onDisconnect(networking::Connection);
-void onConnect(networking::Connection);
+static void OnDisconnect(networking::Connection);
+static void OnConnect(networking::Connection);
+static std::string getConfigurationFilePath(int, char* []);
+static void checkValidConfigurationFile(const nlohmann::json&);
 
-int main(int argc, const char* argv[]){
+int main(int argc, char* argv[]){
 	
-	const std::string SERVER_CONFIGURATION_FILE_LOCATION = "config/ServerProperties.json";
-
 	//Read Json
-	std::ifstream configurationFile(SERVER_CONFIGURATION_FILE_LOCATION, std::ifstream::in);
-	
+	std::ifstream configurationFile(getConfigurationFilePath(argc, argv), std::ifstream::in);
+
 	nlohmann::json configuration = nlohmann::json::parse(configurationFile);
 
-	std::string port = configuration["DefaultPort"];
+	checkValidConfigurationFile(configuration);
+
+	unsigned short port = configuration["DefaultPort"];
 	std::string htmlpath = configuration["HTML Location"];
 
 	std::cout << "starting server \nport: " << port << "\nhtml path: " << htmlpath << '\n';
 
 	//Configure Server
-	//string to short conversion check
-	//html path check for valid file
-    networking::Server server{std::stoi(port), htmlpath, OnConnect, OnDisconnect};
-
-	//Select Game
-    std::string games[] {"game1.json", "game2.json", "game3.json"}; //keep this in json as well?
-    std::cout << "\nPlease select a game\n1. Game 1\n2. Game 2\n3. Game 3\n";
-    int gameIndex;
-
-    do{
-    	std::cin >> gameIndex;
-	}while(gameIndex <= 0 || gameIndex > 3);
-
- 	std::cout << "Selected Game: " << games[gameIndex-1] << "\n";
-
-    //Prepare Lobby in background
-
-    //wait for "start game"?
-
-    //get all users
-
-    //start game with users
-
-	//Wait until End signal
-
-	//select another game
+    networking::Server server{port, htmlpath, OnConnect, OnDisconnect};
 
 	return 0;
 }
 
 //teacher provided functions
-void
-OnConnect(networking::Connection c) {
+static void OnConnect(networking::Connection c) {
   std::cout << "New connection found: " << c.id << "\n";
   clients.push_back(c);
 }
 
 //teacher provided functions
-void
-OnDisconnect(networking::Connection c) {
+static void OnDisconnect(networking::Connection c) {
   std::cout << "Connection lost: " << c.id << "\n";
   auto eraseBegin = std::remove(std::begin(clients), std::end(clients), c);
   clients.erase(eraseBegin, std::end(clients));
+}
+
+static std::string getConfigurationFilePath(int argc, char* argv[]){
+	if(argc <= 1){
+		return SERVER_CONFIGURATION_FILE_LOCATION;
+		//TODO: if config file does not exist, create a default one
+	}
+	else{
+		return argv[1];
+	}
+}
+
+static void checkValidConfigurationFile(const nlohmann::json &configurationFile) {
+	
+	auto port = configurationFile.at("DefaultPort");
+	std::string htmlpath = configurationFile.at("HTML Location");
+
+	//string to short conversion check
+	if(port.get<unsigned short>() != port.get<std::intmax_t>()){
+		std::cout << "Port out of range\n";
+	    std::exit(-1);
+	}
+
+	//html path check for valid file
+	if(access(htmlpath.c_str(), R_OK) == -1){
+	    std::cout << "Unable to open HTML index file: " << htmlpath << "\n";
+        std::exit(-1);
+	}
+	
 }
