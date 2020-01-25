edf0671866170c8e79aa401a5f61e482257a96e9
dbui <dbui@sfu.ca>
2020-01-24 20:16:09 -0800

    commit player

--- src/player/Player.cpp
+++ src/player/Player.cpp
@@ -1,9 +1,6 @@
 #include "include/Player.h"
 
-Player::Player (std::string _name, std::string _id, Invitation& invitation1, const std::string& status) : User(_name, _id) {
-    _invitation = &invitation1;
-    _status = status;
-}
+    
 
 void Player::setInvitation(Invitation& invitation1){
     _invitation = &invitation1;
@@ -22,7 +19,16 @@ std::string Player::getStatus() const{
 }
 
 void Player::addFriend(const User& user){
-    _friendlist.push_back(user);
+    bool contain = false;
+    for (std::size_t i = 0; i < _friendlist.size(); i++){
+        if(_friendlist[i].getId == user.getId())
+            contain = true;
+            break;
+    }
+    
+    if (contain = false)
+        _friendlist.push_back(user);
+    
 }
 
 void Player::unfriend(std::string id){
