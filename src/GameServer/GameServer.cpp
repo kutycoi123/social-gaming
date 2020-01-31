3e925b0f9220a44a7abbe3f198ff0ea777a0ef4b
JadeRabbit <yuheng_wu@sfu.ca>
2020-01-30 22:06:46 -0800

    adding support for multiple views

--- src/GameServer/GameServer.cpp
+++ src/GameServer/GameServer.cpp
@@ -8,24 +8,35 @@
 #include <string>
 #include <unistd.h>
 #include <thread>
-#include <ctime>
+#include <vector>
+
+struct MessageInfo{
+	networking::Message message;
+	//information about which lobby to send message to
+};
 
 static std::atomic<bool> exit_thread_flag{false};
 static const std::string SERVER_CONFIGURATION_FILE_LOCATION = "config/ServerProperties.json";
 static std::vector<networking::Connection> clients;
 
+//main thread
 static void OnDisconnect(networking::Connection);
 static void OnConnect(networking::Connection);
-static std::string processMessages(networking::Server&, const std::deque<networking::Message>&);
-static std::deque<networking::Message> buildOutgoing(const std::string&);
 static std::string getConfigurationFilePath(int, char* []);
 static void checkValidConfigurationFile(const nlohmann::json&);
+
+//message thread
 static void handleMessages(networking::Server&);
+static std::vector<MessageInfo> processMessages(networking::Server&, const std::deque<networking::Message>&);
+static std::deque<networking::Message> buildOutgoing(const std::vector<MessageInfo>&);
+
+
 
 int main(int argc, char* argv[]){
 	
 	//Read Json
-	std::ifstream configurationFile(getConfigurationFilePath(argc, argv), std::ifstream::in);
+	std::string configurationFilePath = getConfigurationFilePath(argc, argv);
+	std::ifstream configurationFile(configurationFilePath, std::ifstream::in);
 
 	nlohmann::json configuration = nlohmann::json::parse(configurationFile);
 
@@ -97,6 +108,8 @@ static void checkValidConfigurationFile(const nlohmann::json &configurationFile)
 	
 }
 
+#pragma region ClientServerNetworkingThread
+
 static void handleMessages(networking::Server& server){
 	
 	while (exit_thread_flag == false) {
@@ -113,8 +126,10 @@ static void handleMessages(networking::Server& server){
 		}
 
 		auto incoming = server.receive();
-		std::string log = processMessages(server, incoming);
-		auto outgoing = buildOutgoing(log);
+		std::vector<MessageInfo> returnMessage = processMessages(server, incoming);
+
+		auto outgoing = buildOutgoing(returnMessage);
+
 		server.send(outgoing);
 
 		if (errorWhileUpdating) {
@@ -127,12 +142,10 @@ static void handleMessages(networking::Server& server){
 	return;
 }
 
-#pragma region ClientServerNetworkingThread
+static std::vector<MessageInfo> processMessages(networking::Server& server, const std::deque<networking::Message>& incoming) {
+	std::vector<MessageInfo> result;
 
-static std::string processMessages(networking::Server& server, const std::deque<networking::Message>& incoming) {
-	std::ostringstream result;
-
-	for (auto& message : incoming) {
+	for (networking::Message message : incoming) {
 		std::cout << message.connection.id << "> " << message.text << "\n";
 
 		if (message.text == "quit") {
@@ -148,20 +161,48 @@ static std::string processMessages(networking::Server& server, const std::deque<
 		else if (message.text == "create lobby"){
 			std::cout << "creating lobby\n";
 		}
+		else if (message.text == "join lobby"){
+			std::cout << "joining lobby\n";
+		}
 		else {
-			result << message.connection.id << "> " << message.text << "\n";
+			//find session based on connection id
+			//send message into game
+
+			//for example something 
+			//game[connection].message = blahblahblah
+		
+			result.push_back(MessageInfo{networking::Message{message.connection, message.text}});
 		}
 	}
-	return result.str();
+
+	//update all games
+
+	//process messages based on game logic
+
+	//get reply of all games
+
+	return result;
 }
 
 //teacher provided functions
-static std::deque<networking::Message> buildOutgoing(const std::string& log) {
-  std::deque<networking::Message> outgoing;
-  for (auto client : clients) {
-    outgoing.push_back({client, log});
-  }
-  return outgoing;
+static std::deque<networking::Message> buildOutgoing(const std::vector<MessageInfo>& returnMessage) {
+	std::deque<networking::Message> outgoing;
+
+	//only push to clients if they are in the same lobby
+
+	//dummy code
+	std::ostringstream log;
+
+	for(MessageInfo message : returnMessage){
+		networking::Message rawMessage = message.message;
+		log << rawMessage.connection.id << "> " << rawMessage.text << '\n';
+	}
+
+	for (auto client : clients) {
+		outgoing.push_back({client, log.str()});
+	}
+
+	return outgoing;
 }
 
 #pragma endregion
