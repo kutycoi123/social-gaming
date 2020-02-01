985d8e750acf3edcea8e0b696de5a86897b322db
djlin <daniel_jui-che_lin@sfu.ca>
2020-01-31 21:40:11 -0800

    Undo changes to GameSession header

--- src/sessions/include/GameSession.h
+++ src/sessions/include/GameSession.h
@@ -4,8 +4,8 @@
 #include <iostream> 
 #include <string>
 #include <list>
-#include "../invitation/include/Invitation.h"
-#include "../player/include/Player.h"
+#include "Invitation.h"
+
 class GameSession { 
     
 public:
@@ -18,26 +18,15 @@ public:
     void removeAllUsersfromSession();
     size_t totalPlayerCount();
     int sessionConfigureSettings(std::string jsonSettings);
-    void createInviteCode()
-    std::list<Player> GetPlayers();
-    
 
 private:
- 
-    std::list<string> _ListSettingVars;
-    std::list<string> _ListPerAudience;
-    std::list<string> _ListRules;
     std::string _currentGame;
-    std::list<int> _playersIDInSession;
-
-    std::list<Player> _playersList;
-
+    std::list<int> _playersInSession;
     std::string _sessionName;
     Invitation _invitationCode;
     int _gameID;
     int _ownerID;
     std::string _JSONSetting;
-    int _totalNumPlayers;
 
 };
 
