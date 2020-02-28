e6c1c4f3125b8681fef48547e9dcb5b6f8e822eb
Joshua Deans <jdeans@sfu.ca>
2020-02-27 18:49:49 -0800

    Added getter methods to gamestate

--- src/GameState/GameState.cpp
+++ src/GameState/GameState.cpp
@@ -1,9 +1,9 @@
 #include "GameState.h"
+#include <optional>
 
 GameState::GameState(GameSpecification::GameSpec& gameSpec) :
     gameSpec(gameSpec),
     gameStarted(false),
-    // The following methods are supposed to make copies. I wanted to confirm if they actually do that.
     constantsMap(gameSpec.getConstants()),
     variablesMap(gameSpec.getVariables()),
     perPlayerMap(gameSpec.getPerPlayer()),
@@ -16,4 +16,32 @@ void GameState::startGame() {
 
 bool GameState::isGameStarted() const {
     return gameStarted;
+}
+
+std::optional<std::reference_wrapper<GameSpecification::SpecValue>> GameState::getConstant(const std::string& key) {
+    auto it = constantsMap.find(key);
+    return (it != constantsMap.end()) ?
+        std::optional<std::reference_wrapper<GameSpecification::SpecValue>>{it->second} :
+        std::nullopt;
+}
+
+std::optional<std::reference_wrapper<GameSpecification::SpecValue>> GameState::getVariables(const std::string& key) {
+    auto it = variablesMap.find(key);
+    return (it != variablesMap.end()) ?
+           std::optional<std::reference_wrapper<GameSpecification::SpecValue>>{it->second} :
+           std::nullopt;
+}
+
+std::optional<std::reference_wrapper<GameSpecification::SpecValue>> GameState::getPerPlayerValue(const std::string& key) {
+    auto it = perPlayerMap.find(key);
+    return (it != perPlayerMap.end()) ?
+           std::optional<std::reference_wrapper<GameSpecification::SpecValue>>{it->second} :
+           std::nullopt;
+}
+
+std::optional<std::reference_wrapper<GameSpecification::SpecValue>> GameState::getPerAudienceValue(const std::string& key) {
+    auto it = perAudienceMap.find(key);
+    return (it != perAudienceMap.end()) ?
+           std::optional<std::reference_wrapper<GameSpecification::SpecValue>>{it->second} :
+           std::nullopt;
 }
--- src/GameState/include/GameState.h
+++ src/GameState/include/GameState.h
@@ -8,10 +8,15 @@ public:
     explicit GameState(GameSpecification::GameSpec&);
     bool isGameStarted() const;
     void startGame();
+    std::optional<std::reference_wrapper<GameSpecification::SpecValue>> getConstant(const std::string&);
+    std::optional<std::reference_wrapper<GameSpecification::SpecValue>> getVariables(const std::string&);
+    std::optional<std::reference_wrapper<GameSpecification::SpecValue>> getPerPlayerValue(const std::string&);
+    std::optional<std::reference_wrapper<GameSpecification::SpecValue>> getPerAudienceValue(const std::string&);
 private:
     bool gameStarted;
     std::unordered_map<std::string, GameSpecification::SpecValue> constantsMap;
     std::unordered_map<std::string, GameSpecification::SpecValue> variablesMap;
+    // Will change perPlayer/perAudience impl to be different from constantsMap/variablesMap
     std::unordered_map<std::string, GameSpecification::SpecValue> perPlayerMap;
     std::unordered_map<std::string, GameSpecification::SpecValue> perAudienceMap;
     GameSpecification::GameSpec& gameSpec;
