7b557002c0e2304dc4d95fe442047413b005fe3f
djlin <daniel_jui-che_lin@sfu.ca>
2020-03-06 22:28:49 -0800

    Adding Visitor for Add Rule

--- src/GameSpec/Add.cpp
+++ src/GameSpec/Add.cpp
@@ -1,4 +1,6 @@
 #include "Add.h"
+#include <boost/variant/apply_visitor.hpp>
+#include <iostream>
 
 using GameSpecification::Add;
 using GameSpecification::SpecValue;
@@ -16,21 +18,51 @@ SpecValue Add::getValue() const{
     return value;
 }
 
+class AddVisitor : public boost::static_visitor<>{
+    public:
+    void operator()(int& existing) const {
+        std::cout << "Int\n";
+    }
+    void operator()(double& d) const{
+        std::cout << "Double Visitor\n";
+    }
+    void operator()(std::string& s) const{
+        std::cout << "std::string Visitor\n";
+    }
+    void operator()(bool& b) const{
+        std::cout << "Bool Visitor\n";
+    }
+    void operator()(std::vector<std::string>& sv) const{
+        std::cout << "String Vector Visitor\n";
+    }
+    void operator()(std::unordered_map<std::string, std::string>& um) const{
+        std::cout << "UMap Visitor\n";
+    }
+    void operator()(std::vector<ValueType>& vvt) const{
+        std::cout << "Unordered Variant Vector Visitor\n";
+    }
+    void operator()(std::unordered_map<std::string, ValueType>& umvt) const{
+        std::cout << "Unordered Variant Map Visitor\n";
+    }
+};
+
 void Add::process(GameState& gameState){
-	// TODO: Add code to process add rule
     // TODO: Find a better way to wrap and unwrap boost variant
     // TODO: Handle cases not just for int
     auto a = gameState.getVariable(to);
     if (!a.has_value()){
         return;
     }
-    if (auto retrievedValue = a->lock()) {
-        auto existingValue = boost::get<int>(this->value.value);
-        auto newValue = boost::get<int>(retrievedValue->value);
-        boost::variant<std::string, int, bool, double> result = newValue + existingValue;
 
-        retrievedValue->value = result;
+    if (auto retrievedValue = a->lock()) {
+        auto existingValue = this->value.value;
+
+        auto newValue = retrievedValue->value;
+        // retrievedValue->value = newValue + existingValue;
+        
+        boost::apply_visitor(AddVisitor(), existingValue);
     }
+
 }
 void Add::parseRule(const json& ruleJson){
     try{
