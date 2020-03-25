8d35918252ff7772ab9407ae5d2e217b2a3ab2e8
Joshua Deans <jdeans@sfu.ca>
2020-03-25 11:02:26 -0700

    Made various improvements to GameState, including improving adding values to gamestate, and make a function that returns either a constant or variable value

--- src/GameState/include/GameState.h
+++ src/GameState/include/GameState.h
@@ -6,7 +6,14 @@
 #include "User.h"
 #include "GameConfig.h"
 #include "StateValue.h"
+#include "StateValueBoolean.h"
+#include "StateValueNumber.h"
+#include "StateValueString.h"
+#include "StateValueList.h"
+#include "StateValueMap.h"
+#include <optional>
 
+// TODO: Remove this type
 using ValueType = boost::make_recursive_variant<std::string, int,
         bool, double, std::vector<boost::recursive_variant_>,
         std::unordered_map<std::string, boost::recursive_variant_>>::type;
@@ -17,8 +24,15 @@ public:
     bool isGameStarted() const;
     void startGame();
     void endGame();
+    enum ValueType {
+        CONSTANT,
+        VARIABLE,
+        PER_PLAYER,
+        PER_AUDIENCE
+    };
 
-    std::optional<std::weak_ptr<StateValue>> getConstant(const std::string& key);
+    std::optional<std::weak_ptr<const StateValue>> getConstant(const std::string& key);
+    std::optional<std::weak_ptr<const StateValue>> getConstantOrVariable(const std::string& key);
     std::optional<std::weak_ptr<StateValue>> getVariable(const std::string& key);
     std::optional<std::weak_ptr<StateValue>> getPerPlayerValue(const std::string& key);
     std::optional<std::weak_ptr<StateValue>> getPerAudienceValue(const std::string& key);
@@ -26,19 +40,22 @@ public:
     void addConfig(const GameConfig& config);
 
     // These methods should only be called when GameState is begin constructed in the parser
-    // TODO: Find out to how set up state value objects
-    void addConstant(const std::string& key, const StateValue& value);
-    void addVariable(const std::string& key, const StateValue& value);
-    void addPerPlayer(const std::string& key, const StateValue& value);
-    void addPerAudience(const std::string& key, const StateValue& value);
+    void addValue(const std::string& key, StateValueBoolean value, const ValueType& valueType);
+    void addValue(const std::string &key, StateValueNumber value, const ValueType& valueType);
+    void addValue(const std::string &key, StateValueString value, const ValueType& valueType);
+    void addValue(const std::string &key, StateValueList value, const ValueType& valueType);
+    void addValue(const std::string &key, StateValueMap value, const ValueType& valueType);
 private:
     bool gameStarted;
-    std::unordered_map<std::string, std::shared_ptr<StateValue>> constantsMap;
+    std::unordered_map<std::string, std::shared_ptr<const StateValue>> constantsMap;
     std::unordered_map<std::string, std::shared_ptr<StateValue>> variablesMap;
     // TODO: Change perPlayer/perAudience impl to be different from constantsMap/variablesMap
     std::unordered_map<std::string, std::shared_ptr<StateValue>> perPlayerMap;
     std::unordered_map<std::string, std::shared_ptr<StateValue>> perAudienceMap;
     GameConfig gameConfig;
+
+    void insertIntoCorrectMap(const GameState::ValueType &valueType,
+                              std::pair<std::string, std::shared_ptr<StateValue>>& pair);
 };
 
 #endif
